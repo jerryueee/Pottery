da5d921c58343305d1478f9788bac911cffba60f
Yuchao Jin <3485926866@qq.com>
2025-01-10 16:32:07 +0800

    Discriminator_complete

--- utils/model.py
+++ utils/model.py
@@ -10,8 +10,28 @@ For bonus questions you may need to preserve some interfaces such as more dims,
 conditioned / unconditioned control, etc.
 '''
 import torch
+import torch.nn as nn
+
+
+class DBlock(nn.Module):
+    def __init__(self, inchannel, outchannel, pad_or_not:bool):
+        super().__init__()
+        if pad_or_not:
+            self.pad = 1
+        else:
+            self.pad = 0
+        self.func = nn.Sequential(
+            nn.Conv3d(inchannel, outchannel, kernel_size=4, stride=2, padding=self.pad),
+            nn.BatchNorm3d(outchannel),
+            nn.LeakyReLU(0.2)
+        )
+
+    def forward(self, x):
+        out = self.func(x)
+        return out
 
 class Discriminator(torch.nn.Module):
+
     def __init__(self, resolution=64):
         # initialize superior inherited class, necessary hyperparams and modules
         # You may use torch.nn.Conv3d(), torch.nn.sequential(), torch.nn.BatchNorm3d() for blocks
@@ -19,15 +39,27 @@ class Discriminator(torch.nn.Module):
         # REMENBER YOU ARE WRITING A DISCRIMINATOR (binary classification) so Sigmoid
         # Dele return in __init__
         # TODO
-        return
-    
+        self.resolution = resolution
+        super().__init__()
+        self.func = nn.Sequential(
+            DBlock(1, resolution, pad_or_not=True),
+            DBlock(resolution, resolution * 2, pad_or_not=True),
+            DBlock(resolution * 2, resolution * 4, pad_or_not=True),
+            DBlock(resolution * 4, resolution * 8, pad_or_not=True),
+            # DBlock(resolution * 8, 1, pad_or_not=False),
+            nn.Conv3d(resolution * 8, 1, kernel_size=4, stride=2, padding=0),
+        )
     
     def forward(self, x):
         # Try to connect all modules to make the model operational!
         # Note that the shape of x may need adjustment
         # # Do not forget the batch size in x.dim
         # TODO
-        return out
+        x = x.view((-1, 1, self.resolution, self.resolution, self.resolution))
+        x = self.func(x)
+        x = x.view(-1, 1) # 一个竖列向量
+        out = nn.Sigmoid(x)
+        return out # 输出值在0-1之间
         
     
 class Generator(torch.nn.Module):
